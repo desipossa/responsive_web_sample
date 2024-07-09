54f74f0e8245a58ac2011ce44ae6551bfb4353a6
desipossa <c1jang@hanmail.net>
2024-07-09 14:15:02 +0900

    swiper 051 coffee

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,68 @@ $(function () {
 
     $(window).on('resize', function () {
         $('#header .gnb').removeClass('on');
+        $('#header .mbtn').removeClass('on');
         $('#header .gnb>ul>li ul').removeAttr('style')
     })
+});
+
+$(function () {
+
+    $('.mainVisualSlide').on('init afterChange', function () {
+        const current = $('.mainVisualSlide .slick-current');
+        current.addClass('on').siblings().removeClass('on')
+    })
+
+    $('.mainVisualSlide').slick({
+        userTransform: false,
+        autoplay: true,
+    });
+
+    $('.mainEventSlide').slick({
+        slidesToShow: 4,
+
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+});
+
+$(function () {
+    const mainVisualSlide2 = new Swiper('.mainVisualSlide2', {
+        loop: true,
+        slideActiveClass: 'on'
+    });
+
+    const mainEventSlide2 = new Swiper('.mainEventSlide2', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        breakpoints: {
+            769: {
+                slidesPerView: 4,
+            }
+        }
+    });
+
+
+    const main051Slide = new Swiper('.main051Slide', {
+        direction: "vertical",
+        loop: true,
+        speed: 2000,
+        slidesPerView: "auto",
+        centeredSlides: true,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+    })
 })
+
+
+
